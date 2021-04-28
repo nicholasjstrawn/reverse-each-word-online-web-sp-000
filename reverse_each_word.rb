f37f9de76fba7b3fb635c9361ddcd37db5f6b41b
Nicholas Strawn <nicholasjstrawn@gmail.com>
2021-04-28 00:53:39 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,15 @@
+#def reverse_each_word(string)
+  #reverse_string = string.split
+  #reverse_string.each do |word|
+    #word.reverse!
+  #end
+  #reverse_string.join(" ")
+#end
+
+def reverse_each_word(string)
+  reverse_string = string.split
+  reverse_string.collect do |word|
+    word.reverse!
+  end
+  reverse_string.join(" ")
+end
